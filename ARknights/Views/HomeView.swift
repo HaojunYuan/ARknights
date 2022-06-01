8af33c3a33163dbb9f2bf8234ee5d526270b4acf
Haojun Yuan <haojundev@gmail.com>
2022-05-31 20:30:56 -0400

    Created CardView Component

---
+++ ARknights/Views/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  ARknights
+//
+//  Created by Brian on 2022/5/31.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            HomeView()
+        }
+    }
+}
